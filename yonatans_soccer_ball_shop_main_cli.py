f512c7166b1f9ecc69201e217d8e9c991a08a71d
yogetachew <yonatangetachew14@gmail.com>
2023-09-14 15:13:18 -0600

    final work for part 4

--- yonatans_soccer_ball_shop_main_cli.py
+++ yonatans_soccer_ball_shop_main_cli.py
@@ -11,11 +11,18 @@ def get_input():
         number_of_soccer_balls = int(input("Enter number of soccer balls: "))
         return number_of_soccer_balls
 
+def display():
+        # TODO: Display transaction for customer
+        number_of_soccer_balls = soccerball.SoccerballHeaven.number_of_soccer_balls
+        total_sale = soccerball.SoccerballHeaven.total_sale
+        print(f"Number of soccer balls: {number_of_soccer_balls}")
+        print(f"Your total cost was: ${total_sale:,.2f}")
+
+number_of_soccer_balls = get_input()
+
 yonatans_soccerball = soccerball.SoccerballHeaven()
 
-number_of_soccer_ball = get_input()
-
-yonatans_soccerball.calculate()
+yonatans_soccerball.calculate(number_of_soccer_balls)
 
 yonatans_soccerball.display()
 
